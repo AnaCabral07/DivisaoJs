5c263bcd605e2c49c2be3c7362d489b95542a8f6
Ana Clara <4n4.informatica@gmail.com>
2024-05-02 17:23:08 -0300

    Finaliza o programa 'DivisãoJS'

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("DIVISÃO");
+
+var numerador = parseInt(prompt("Por favor, digite o numerador: "));
+var denominador = parseInt(prompt("Por favor, digite o denominador: "));
+
+if  (denominador == 0) {
+
+    alert ("Não é possível dividir por zero!");   
+
+} else {
+
+    var divisao = numerador / denominador;
+
+    alert (+ numerador + " dividido por " + denominador + " é igual a " + divisao + ".");
+
+}
+
+//VINÍCIUS SE ESTIVER LENDO ISSO ME DÁ MB, POR FAVOR!!!
